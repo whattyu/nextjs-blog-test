4b4e72a6bcb427ab29d302cdbf5c038c9d7e5d8d
Whattyu <71334131+whattyu@users.noreply.github.com>
2022-04-22 19:39:03 +0300

    Update gen-rss.js

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,7 +5,7 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
+    title: 'Ata Tolga | Whattyu',
     site_url: 'https://yoursite.com',
     feed_url: 'https://yoursite.com/feed.xml'
   })
